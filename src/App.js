a056bce4821bd088ee741ccd72c780ac534ccf43
Alex Wade <alex.wade13@gmail.com>
2020-11-22 16:15:33 -0800

    complete?

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import products from './challenge_products.json'
+
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
 
 function App() {
+
+  console.log(products)
+
+  let item_texts = ['GRAVITY BLANKET',
+    'MODERNIST X GRAVITY FLANNEL SHEETS', 
+    'COOLING BLANKET', 
+    'MODERNIST X GRAVITY WEIGHTED ROBE', 
+    'WEIGHTED SLEEP MASK', 
+    'GRAVITY KIDS', 
+    'GRAVITY BAMBOO SHEETS', 
+    'FAUX FUR DUVET COVERS', 
+    'FLANNEL- SHERPA WEIGHTED THROW', 
+    'Z BY GRAVITY WEIGHTED THROW']
+  
+  let items = []
+
+  products.products.forEach((item, i) => {
+    items.push(
+      <Slide className="slide" index={i}>
+        <img 
+          className="item" 
+          src={item.images.[0].src}
+          onMouseOver={e => (e.currentTarget.src =item.images.[1].src)}
+          onMouseOut={e => (e.currentTarget.src = item.images.[0].src)}
+
+
+        />
+        <div className="name">{item.title.toUpperCase()}</div>
+        <div className="price">From ${item.variants[0].price.split('.')[0]}</div> 
+      </Slide>)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CarouselProvider
+          naturalSlideWidth={300}
+          naturalSlideHeight={400}
+          totalSlides={products.products.length}
+          visibleSlides={4}
+          infinite
+        > 
+          <div className="buttons">
+            <ButtonBack className="button-back"/>
+            <ButtonNext className="button-next"/>
+          </div>
+          <div className="title">SHOP BEST SELLERS</div>
+          <Slider>
+            {items}
+          </Slider>
+        </CarouselProvider>
+
+
       </header>
     </div>
   );
